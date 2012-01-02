4ce5308a12de763ee288e1bb271b184d85c33e7c
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-01-02 06:16:31 +0000

    First check for a Unicode BOM and set a tentative encoding if found.

---
+++ DetectEncoding/EncodingDetector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DetectEncoding
+{
+    public class EncodingDetector
+    {
+        private const string Bom = "\ufeff";
+
+        private static readonly IEnumerable<KeyValuePair<byte[], Encoding>> ByteOrderMarks =
+            new[]
+                {
+                    GetByteOrderMark(Encoding.UTF32), // UTF32-LE
+                    GetByteOrderMark(Encoding.GetEncoding(12001)), // UTF32-BE
+                    GetByteOrderMark(Encoding.Unicode), // UTF16-LE
+                    GetByteOrderMark(Encoding.BigEndianUnicode), // UTF16-BE
+                    GetByteOrderMark(Encoding.UTF8)
+                };
+
+        private static KeyValuePair<byte[], Encoding> GetByteOrderMark(Encoding encoding)
+        {
+            return new KeyValuePair<byte[], Encoding>(encoding.GetBytes(Bom), encoding);
+        }
+
+        public Encoding DetectEncoding(byte[] text)
+        {
+            Encoding tentativeEncoding = null;
+
+            foreach (var byteOrderMark in ByteOrderMarks)
+            {
+                if (text.Length < byteOrderMark.Key.Length)
+                {
+                    continue;
+                }
+
+                for (var i = 0; i < byteOrderMark.Key.Length; ++i)
+                {
+                    if (text[i] != byteOrderMark.Key[i])
+                    {
+                        continue;
+                    }
+                }
+
+                tentativeEncoding = byteOrderMark.Value;
+                break;
+            }
+
+
+        }
+    }
+}
