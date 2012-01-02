cc046e183077655b8e3e074826102c8686efe59d
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-01-02 17:45:25 +0000

    Add simple UTF-32 detector.

---
+++ DetectEncoding/Detectors/Utf32Detector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DetectEncoding.Detectors
+{
+    internal class Utf32Detector : IDetector
+    {
+        private const int CodePointProbability = (int) ((long) 1112064*Detector.MaxCertainty/4294967296);
+
+        private readonly bool _bigEndian;
+        private int _codePoint;
+        private int _byteIndex;
+        private int _uncertainty = Detector.MaxCertainty;
+
+        public Utf32Detector(bool bigEndian)
+        {
+            IsValid = true;
+            _bigEndian = bigEndian;
+        }
+
+        public void Consume(byte b)
+        {
+            if (!IsValid)
+            {
+                throw new InvalidOperationException("Utf32Detector.Consume called while invalid");
+            }
+
+            if (_bigEndian)
+            {
+                _codePoint |= b << ((3 - _byteIndex)*8);
+            }
+            else
+            {
+                _codePoint |= b << (_byteIndex*8);
+            }
+
+            if (++_byteIndex == 4)
+            {
+                if ((_codePoint >= 0xd800 && _codePoint <= 0xdfff) || _codePoint > 0x10fff)
+                {
+                    IsValid = false;
+                }
+                else
+                {
+                    _uncertainty = (_uncertainty*CodePointProbability) >> Detector.MaxCertaintyBits;
+                }
+                _byteIndex = 0;
+            }
+        }
+
+        public bool IsValid { get; private set; }
+
+        public int Certainty { get { return Detector.MaxCertainty - _uncertainty; } }
+    }
+}
